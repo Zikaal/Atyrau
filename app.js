b4feadb7622f927e90b956e9b02901eae7582ad2
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-02-01 19:44:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const bounds = [
+    [45.5, 46.5],  // Юго-западная граница (ещё левее)
+    [49.5, 56.0]   // Северо-восточная граница (ещё правее)
+  ];
+  
+  const map = L.map('map', {
+    center: [47.5, 52.5], // Центр карты на Атырау
+    zoom: 7, // Уменьшенный стартовый зум
+    minZoom: 7, // Минимальный зум (не даёт отдаляться слишком сильно)
+    maxBounds: bounds, // Ограничение границ карты
+    maxBoundsViscosity: 1.0 // Полностью ограничивает выход за пределы области
+  });
+  
+  // Подключение OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+  
+  // Загрузка границ Атырауской области (GeoJSON)
+  fetch('atyrau_region.geojson')
+    .then(response => response.json())
+    .then(data => {
+      // Отображаем область с контурами
+      const regionLayer = L.geoJSON(data, {
+        style: {
+          color: '#ff7800', // Оранжевые границы
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.1
+        }
+      }).addTo(map);
+  
+      // Автоматическая подгонка карты под границы области
+      map.fitBounds(regionLayer.getBounds());
+    })
+    .catch(error => {
+      console.error('Ошибка загрузки GeoJSON:', error);
+    });
+  
+  // Добавление маркеров для ключевых точек области
+  L.marker([47.1167, 51.8833]).addTo(map)
+    .bindPopup("<b>Атырау</b><br>Столица области.");
+  
+  L.marker([47.0667, 52.1667]).addTo(map)
+    .bindPopup("<b>Кульсары</b><br>Город в Атырауской области.");
+  
+  L.marker([46.9500, 51.5500]).addTo(map)
+    .bindPopup("<b>Карабатан</b><br>Промышленный район.");
+  
