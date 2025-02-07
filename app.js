8d79a45f61998a6a36bea28ddef46d561f1f890b
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-02-07 11:01:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 const bounds = [
     [45.5, 46.5],  // Юго-западная граница (ещё левее)
     [49.5, 56.0]   // Северо-восточная граница (ещё правее)
-  ];
-  
-  const map = L.map('map', {
-    center: [47.5, 52.5], // Центр карты на Атырау
-    zoom: 7, // Уменьшенный стартовый зум
-    minZoom: 7, // Минимальный зум (не даёт отдаляться слишком сильно)
-    maxBounds: bounds, // Ограничение границ карты
-    maxBoundsViscosity: 1.0 // Полностью ограничивает выход за пределы области
-  });
-  
-  // Подключение OpenStreetMap
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-  }).addTo(map);
-  
-  const bounds = [
-    [45.5, 46.5],  // Юго-западная граница (ещё левее)
-    [49.5, 56.0]   // Северо-восточная граница (ещё правее)
 ];
 
 const map = L.map('map', {
